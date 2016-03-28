2ddbdcfe1c6f25062201c880f8ad59e80ea8ae52
christianolorenzo <christiano.lorenzo@gmail.com>
2016-03-28 00:22:05 -0500

    modify array.c

--- array.c
+++ array.c
@@ -4,13 +4,15 @@ int main(int argc, char *argv[])
 {
     int num[6];
     int counter;
+    int sum = 0;
 
     for(counter = 0; counter < 6; counter++){
         printf("Enter an Integer: ");
         scanf("%d", &num[counter]);
+        sum = sum + num[counter];
     }
     
-    printf("%d", num[counter]);
+    printf("The sum is: %d \n", sum);
 
     return 0;
 
